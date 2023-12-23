75f7e7f00d37a587eef3eab81a2a73a8cf04c8f4
Mateus Adamo <adamomateus478@gmail.com>
2023-12-23 20:30:01 -0300

    feat: Create of Main.

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { StatusBar } from 'expo-status-bar';
+import { View, Text } from "react-native";
+
+export default function Main() {
+    return (
+        <View >
+            <Text>Hello</Text>
+            <StatusBar style="auto" />
+        </View>
+    )
+}
